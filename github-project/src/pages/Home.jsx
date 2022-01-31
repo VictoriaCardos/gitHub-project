265f384bc0f1b2834732d55fcba36a526ad71ae8
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-30 21:44:22 -0300

    feat: create home page

---
+++ github-project/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainContainer from '../components/containerPrincipal/index'
+
+const Home = () => {
+  return (
+    <MainContainer>
+      <h1>Funcionou!</h1>
+    </MainContainer>
+  )
+}
+
+export default Home
