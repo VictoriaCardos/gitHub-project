4f0b8f46022233813e233d656ff082a5ea0012c5
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-30 21:43:47 -0300

    feat: create container principal

---
+++ github-project/src/components/containerPrincipal/styles.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  height: 85vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background: var(--container);
+`
