61cdf911946cfe0660e3f31cc100a93966b0f24d
VictoriaCardos <vvjcerque@hotmail.com>
2022-02-01 19:12:52 -0300

    feat: add user container

--- github-project/src/pages/Home.jsx
+++ github-project/src/pages/Home.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { context } from '../context/index'
+
 import MainContainer from '../components/containerPrincipal/index'
 import Header from '../components/Header/index'
+import UserContainer from '../components/userContainer/index'
+import UserPicture from '../components/userPicture/index'
+import UserDetails from '../components/userDetails/index'
+import UserNumbers from '../components/userNumbers/index'
 
 const Home = () => {
+  const ctx = useContext(context)
+
   return (
     <MainContainer>
       <Header />
+      <UserContainer>
+        {ctx.userData?.name ? (
+          <>
+            <UserPicture
+              url={ctx.userData?.avatar_url}
+              alternativeText={ctx.userData?.login}
+            />
+
+            <UserDetails
+              name={ctx.userData?.name}
+              bio={ctx.userData?.bio}
+              login={ctx.userData?.login}
+            />
+
+            <UserNumbers
+              repos={ctx.userData?.public_repos}
+              followers={ctx.userData?.followers}
+              following={ctx.userData?.following}
+            />
+          </>
+        ) : undefined}
+      </UserContainer>
     </MainContainer>
   )
 }
