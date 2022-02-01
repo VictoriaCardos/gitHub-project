70d600e64b0bd205797d7f2c338ffa9bc888030f
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-31 22:12:00 -0300

    add context and api

--- github-project/src/components/Header/index.jsx
+++ github-project/src/components/Header/index.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import { context } from '../../context/index'
+import api from '../../services/api'
 
 import {
   HeaderSection,
@@ -9,14 +11,31 @@ import {
 } from './styles'
 
 const Header = () => {
+  const ctx = useContext(context)
+  const [searchedUser, setSearchedUser] = useState('')
+
+  async function getUserData() {
+    try {
+      const response = await api.get(`/${searchedUser}`)
+
+      ctx.setUserData(response.data)
+      console.log(ctx.setUserData(response.data))
+    } catch {
+      alert(`${searchedUser} não foi encontrado`)
+    }
+  }
+
   return (
     <HeaderSection>
       <HeaderTitle>Github Profile</HeaderTitle>
 
       <HeaderInputContainer>
-        <HeaderInput />
+        <HeaderInput
+          value={searchedUser}
+          onChange={e => setSearchedUser(e.target.value)}
+        />
 
-        <HeaderSearchButton></HeaderSearchButton>
+        <HeaderSearchButton onClick={getUserData}></HeaderSearchButton>
       </HeaderInputContainer>
     </HeaderSection>
   )
