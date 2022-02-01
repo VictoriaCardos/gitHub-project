300ba3e88a7643bc54c6fe9aafdbfc802435aa5f
VictoriaCardos <vvjcerque@hotmail.com>
2022-01-31 22:09:25 -0300

    feat: add context

--- github-project/src/App.js
+++ github-project/src/App.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import Routes from './routes'
 import { GlobalStyle } from './styles/global'
+import { ContextProvider } from './context/index'
 
 function App() {
   return (
-    <Router>
-      <GlobalStyle />
-      <Routes />
-    </Router>
+    <ContextProvider>
+      <Router>
+        <GlobalStyle />
+        <Routes />
+      </Router>
+    </ContextProvider>
   )
 }
 
