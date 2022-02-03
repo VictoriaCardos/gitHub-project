eaa49ce4d825fb387a4a29d4dd072657b82cfe6a
VictoriaCardos <vvjcerque@hotmail.com>
2022-02-02 23:12:06 -0300

    feat: add route for repos page

--- github-project/src/routes.js
+++ github-project/src/routes.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Repos from './pages/Repos'
 
 export default function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/repos" element={<Repos />} />
     </Routes>
   )
 }
