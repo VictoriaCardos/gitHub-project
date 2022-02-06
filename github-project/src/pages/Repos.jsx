17187b7430f011a516f141118ee521e7f0ca0487
VictoriaCardos <vvjcerque@hotmail.com>
2022-02-05 21:47:11 -0300

    feat: add container and button for go back

--- github-project/src/pages/Repos.jsx
+++ github-project/src/pages/Repos.jsx
@@ -1,11 +1,20 @@
 import React, { useContext } from 'react'
 
+import Container from '../components/containerPrincipal'
+import ButtonBack from '../components/Button'
+
 import ReposContainer from '../components/reposContainer/index'
 import { context } from '../context/index'
 
 const Repos = () => {
   const ctx = useContext(context)
-  return <ReposContainer name={ctx.userData.name} repos={ctx.repos} />
+
+  return (
+    <Container>
+      <ReposContainer name={ctx.userData.name} repos={ctx.repos} />
+      <ButtonBack />
+    </Container>
+  )
 }
 
 export default Repos
