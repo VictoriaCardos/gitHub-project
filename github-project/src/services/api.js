8eeb0f310ef3860637d667e00958951da108bbff
VictoriaCardos <vvjcerque@hotmail.com>
2022-02-06 18:53:17 -0300

    feat: add headers

--- github-project/src/services/api.js
+++ github-project/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://api.github.com/users'
+  baseURL: 'https://api.github.com/users',
+  headers: {
+    'User-Agent': 'request'
+  }
 })
 
 export default api
