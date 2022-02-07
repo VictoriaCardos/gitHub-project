b5e522e87509d7435ab90f8695857c980abda003
VictoriaCardos <vvjcerque@hotmail.com>
2022-02-07 20:15:09 -0300

    feat: add logic for redirect

--- github-project/src/components/followersContainer/index.jsx
+++ github-project/src/components/followersContainer/index.jsx
@@ -6,23 +6,34 @@ import {
   ProfilePicture
 } from './styles'
 
-import React from 'react'
+import React, { useContext } from 'react'
+import { context } from '../../context'
+import { useNavigate } from 'react-router-dom'
+import api from '../../services/api'
+
+//está acontecendo o que eu queria porem ele está percorrendo e renderizando todos os followers, por isso mudei o onClick para o <h2>, vamos ver o que acontece.
 
 const FollowersContainer = props => {
+  const ctx = useContext(context)
+  const navigate = useNavigate()
+
+  async function redirectForHome(foll) {
+    const response = await api.get(`/${foll.login}`)
+    ctx.setUserData(response.data)
+    navigate('/')
+  }
+
   return (
     <Section>
       <Title>Seguidores de {props.name.split(' ')[0]}</Title>
 
       <ListOfFollowersContainer>
         {(props?.followers).map(foll => (
-          <Foll
-            onClick={() => (window.location.href = foll?.html_url)}
-            id={foll.id}
-          >
+          <Foll key={foll?.id}>
             <ProfilePicture src={foll.avatar_url} alt={foll.avatar_url} />
 
             <span>
-              <h2>{foll?.login}</h2>
+              <h2 onClick={redirectForHome(foll)}>{foll?.login}</h2>
             </span>
           </Foll>
         ))}
