1cb694bbd3c7b91141ce5fb143c4d2451919e60f
VictoriaCardos <vvjcerque@hotmail.com>
2022-02-07 21:52:02 -0300

    feat: add modal

--- github-project/src/App.js
+++ github-project/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Routes from './routes'
 import { GlobalStyle } from './styles/global'
 import { ContextProvider } from './context/index'
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root') //isso é só pra acessibilidade
 
 function App() {
   return (
