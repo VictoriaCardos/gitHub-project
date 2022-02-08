db75728002f5f2c4477db5fc8395a1300e75c188
VictoriaCardos <vvjcerque@hotmail.com>
2022-02-07 21:52:35 -0300

    style: add style of modal

--- github-project/src/styles/global.jsx
+++ github-project/src/styles/global.jsx
@@ -39,4 +39,56 @@ export const GlobalStyle = createGlobalStyle`
   h1,h2,h3,h4,h5,h6,strong{
     font-weight:600;
   }
+
+  .react-modal-overlay{
+    background: transparent;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    backdrop-filter: blur(2px);
+  }
+  .react-modal-content{
+    border: 2px solid ;
+    width: 100%;
+    max-width:576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    button{
+      width: 60px;
+      transition:  0.1s;
+      &:hover{
+        color:var(--background);
+        background: var(--orange);
+        border-radius: 0.25rem;
+      }
+    }
+
+    
+  }
+  .react-modal-close{
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+    transition: filter 0.2s;
+    &:hover{
+      filter: brightness(0.8);
+    }
+  }
+
+  button{
+    cursor: pointer;
+  }
   `
