2ab1da97d0725d5542aec41ad21585fd4226815e
VictoriaCardos <vvjcerque@hotmail.com>
2022-02-09 19:53:18 -0300

    style: add new color

--- github-project/src/components/containerPrincipal/styles.jsx
+++ github-project/src/components/containerPrincipal/styles.jsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components'
 
 export const Container = styled.main`
-  height: 85vh;
+  height: 19vh;
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  background: transparent;
+  background: var(--container);
   margin-top: 20px;
   justify-content: flex-start;
 `
