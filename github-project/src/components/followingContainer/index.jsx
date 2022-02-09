dd315b07997447d79d1ff3d8798289a6a7caf0a2
VictoriaCardos <vvjcerque@hotmail.com>
2022-02-08 21:16:35 -0300

    feat: add logic for redirect

--- github-project/src/components/followingContainer/index.jsx
+++ github-project/src/components/followingContainer/index.jsx
@@ -5,22 +5,41 @@ import {
   Foll,
   ProfilePicture
 } from './styles'
-import React from 'react'
+import React, { useContext } from 'react'
+import api from '../../services/api'
+import { context } from '../../context'
+import { useNavigate } from 'react-router-dom'
 
 const FollowingContainer = props => {
+  const ctx = useContext(context)
+  const navigate = useNavigate()
+
   return (
     <Section>
       <Title>{props.name.split(' ')[0]} está seguindo:</Title>
 
       <ListOfFollowingContainer>
         {props.following.map(foll => (
-          <Foll
-            onClick={() => (window.location.href = foll?.html_url)}
-            key={foll?.id}
-          >
+          <Foll key={foll?.id}>
             <ProfilePicture src={foll.avatar_url} alt={foll.avatar_url} />
             <span>
-              <h2>{foll?.login}</h2>
+              <h2
+                onClick={async function redirectForHome() {
+                  const response = await api.get(`/${foll.login}`)
+                  const repos = await api.get(`/${foll.login}/repos`)
+                  const followers = await api.get(`/${foll.login}/followers`)
+                  const following = await api.get(`/${foll.login}/following`)
+
+                  ctx.setUserData(response.data)
+                  ctx.setRepos(repos.data)
+                  ctx.setFollowersData(followers.data)
+                  ctx.setFollowingData(following.data)
+                  navigate(`/`)
+                  console.log(ctx)
+                }}
+              >
+                {foll?.login}
+              </h2>
             </span>
           </Foll>
         ))}
